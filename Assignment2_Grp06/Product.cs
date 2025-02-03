3dd00f7b470963123204f929b7f3256d8082f193
Nirav04-hub <nsaxena9968@conestogac.on.ca>
2025-02-02 22:34:06 -0500

    Created a Product.cs and ProductTest.cs

--- Assignment2_Grp06/Product.cs
+++ Assignment2_Grp06/Product.cs
@@ -8,5 +8,42 @@ namespace Assignment2_Grp06
 {
     public class Product
     {
+        public int ProdID;
+        public string ProdName;
+        public int ItemPrice;
+        public int StockAmount;
+
+        Product(int ProdID, string ProdName, int ItemPrice, int StockAmount)
+        {
+            this.ProdID = ProdID;
+            this.ProdName = ProdName;
+            this.ItemPrice = ItemPrice;
+            this.StockAmount = StockAmount;
+        }
+
+        public void IncreaseInStock(int Amount)
+        {
+            if (Amount != null && Amount > 0  )
+            {
+                StockAmount += Amount;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Amount");
+            }
+        }
+
+        public void DecreaseInStock(int Amount)
+        {
+            if (Amount != null && Amount > 0 && Amount <= StockAmount)
+            {
+                StockAmount -= Amount;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Amount");
+            }
+        }
+
     }
 }
